c2cc1e0fc77f6f10354ad3eb21b465aa05d05df5
Vanderal Talagi <vanderaltalagi@gmail.com>
2022-06-29 20:40:51 +1200

    commit

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const audioForm = new AudioForm(locationSelect,ambianceForm, {
 })
 
 const audioPlayer = new AudioPlayer(locationSelect);
+
+//Creating the flicker candle light effect
+document.addEventListener(onload, () => {
+    var time = Math.random();
+    var candle = document.querySelector(".ambiance-candle-anim");
+    candle.style.setProperty('--animation-time',time + 's');
+})
+
